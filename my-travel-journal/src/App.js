1a00755d74e62755c1e8ddfd894321cafc1c4396
George Anya <georgeanya4real@gmail.com>
2022-06-27 10:21:36 +0100

    commit

--- my-travel-journal/src/App.js
+++ my-travel-journal/src/App.js
@@ -1,46 +1,72 @@
-import React from 'react';
-import './App.css';
-import Box from './components/box';
+import React from "react";
+import "./App.css";
+import Box from "./components/box";
+import { nanoid } from "nanoid";
 // import Navbar from './components/navbar';
 // import State from './components/state';
 // import TravelCard from './components/travelCard';
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
 
-  const [dice, setDice] = React.useState(allNewDice())
+  React.useEffect(
+    () => {
+
+    }, [dice]
+  )
   
   function holdDice(id) {
-    setDice(oldDice => oldDice.map(die => {
-      return die.id === id ?
-        {...die, isHeld: !die.isHeld} :
-        die
-    }))  
-  }
-
-  function handleClick(){
-    setDice(allNewDice)
-  }
-  const diceElement = dice.map(die => <Box isHeld={die.isHeld} value = {die.value}  holdDice = {() => holdDice(die.id)}/>
-  )
-
-
-  function allNewDice(){
-    const newDice = []
-    for (let i = 0; i < 10; i++){
-      newDice.push({
-        value: Math.ceil(Math.random() * 6), 
-        isHeld: true
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
       })
+    );
+  }
+  function handleClick() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld
+          ? die
+          : {
+              value: Math.ceil(Math.random() * 6),
+              isHeld: false,
+              id: nanoid(),
+            };
+      })
+    );
+  }
+
+  // function handleClick() {
+  //   setDice(allNewDice());
+  // }
+  const diceElement = dice.map((die) => (
+    <Box
+      isHeld={die.isHeld}
+      value={die.value}
+      holdDice={() => holdDice(die.id)}
+      key={die.id}
+    />
+  ));
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
-    return newDice
+    return newDice;
   }
   return (
     <div className="App">
-      <div className='body'>
-        <div className='dice-container'>
-          {diceElement}
-        </div>
-        <button  className='roll-dice' onClick={handleClick}>ROLL DICE</button>
+      <div className="body">
+        <div className="dice-container">{diceElement}</div>
+        <button className="roll-dice" onClick={handleClick}>
+          ROLL DICE
+        </button>
       </div>
     </div>
   );
--- my-travel-journal/src/components/box.jsx
+++ my-travel-journal/src/components/box.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
-
+import React from "react";
 
 const Box = (props) => {
-  
+  const style = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
 
-    const style={
-        backgroundColor: props.isHeld ? '#59E391' : 'none'
-    }
-
-    function toggleIsHeld() {
-      props.holdDice()
-    }
+  function toggleIsHeld() {
+    props.holdDice();
+  }
   return (
-    <div 
-        onClick={toggleIsHeld}
-        style={style} 
+    <div onClick={toggleIsHeld} style={style} className="dice">
+      <h3 className="dice-num">{props.value}</h3>
+    </div>
+  );
+};
 
-        className='dice'
-        
-    ><h3 className='dice-num'>{props.value}</h3></div>
-  )
-}
-
-export default Box
+export default Box;
